9459bc26ca53eec98a4fbb88e6f2f5d37bdfc2b4
Mahmud <me@imahmud.com>
2025-07-24 01:01:58 +0600

    feat(component): add IconButton component

---
+++ app/components/ui/IconButton.tsx
@@ -0,0 +1,23 @@
+import { cn } from '~/utils';
+
+type IconButtonProps = {
+  variant?: 'outline' | 'solid' | 'ghost';
+  className?: string;
+  children: React.ReactNode;
+};
+
+export default function IconButton({ variant = 'solid', className, children }: IconButtonProps) {
+  return (
+    <button
+      className={cn(
+        'inline-flex size-8 shrink-0 cursor-pointer items-center justify-center rounded-lg p-2',
+        variant === 'outline' && 'border border-icon-border',
+        variant === 'solid' && 'bg-primary text-white',
+        variant === 'ghost' && 'bg-transparent',
+        className,
+      )}
+    >
+      {children}
+    </button>
+  );
+}
