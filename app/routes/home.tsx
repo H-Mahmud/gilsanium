6307c6656d781ba3d5fb80ac269cf59f4d54d0b7
Mahmud <me@imahmud.com>
2025-07-23 22:12:10 +0600

    refactor: format the respository with prettier

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,7 @@
-import type { Route } from "./+types/home";
+import type { Route } from './+types/home';
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "Gilsanium | Home" },
-    { name: "description", content: "Welcome to Gilsanium!" },
-  ];
+  return [{ title: 'Gilsanium | Home' }, { name: 'description', content: 'Welcome to Gilsanium!' }];
 }
 
 export default function Home() {
