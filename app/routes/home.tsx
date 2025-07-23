b7789ea31432f38503dcfdf3ef7aeebb980ce856
Mahmud <me@imahmud.com>
2025-07-23 21:59:16 +0600

    chore: initialize react router 7 project setup with TypeScript, Tailwindcss, and environment files

---
+++ app/routes/home.tsx
@@ -0,0 +1,12 @@
+import type { Route } from "./+types/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Gilsanium | Home" },
+    { name: "description", content: "Welcome to Gilsanium!" },
+  ];
+}
+
+export default function Home() {
+  return <h1>Hello World!</h1>;
+}
