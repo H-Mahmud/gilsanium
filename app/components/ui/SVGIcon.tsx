b7936de6d503f5d4eef5e7ef30b56f7645411d0b
Mahmud <me@imahmud.com>
2025-07-24 08:04:38 +0600

    style(component): update SVGIcon icon default size to large

--- app/components/ui/SVGIcon.tsx
+++ app/components/ui/SVGIcon.tsx
@@ -9,7 +9,7 @@ export default function SVGIcon({ src, className, ...other }: SVGIconProps) {
   return (
     <span
       className={cn(
-        'inline-flex size-6 shrink-0 bg-current mask-contain mask-no-repeat',
+        'inline-flex size-12 shrink-0 bg-current mask-contain mask-no-repeat',
         className,
       )}
       style={{
