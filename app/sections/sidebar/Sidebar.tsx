5fe8f271e4826182ed7bf45ca1b1d2b6ec257812
Mahmud <me@imahmud.com>
2025-07-24 16:53:04 +0600

    style: add sticky sidebar

--- app/sections/sidebar/Sidebar.tsx
+++ app/sections/sidebar/Sidebar.tsx
@@ -3,10 +3,11 @@ import Logo from './Logo';
 import SVGIcon from '~/components/ui/SVGIcon';
 import SidebarMenus from './SidebarMenus';
 import { navData } from '~/config-dashboard-menu';
+import ActiveSuperCard from '~/components/ActiveSuperCard';
 
 export default function Sidebar() {
   return (
-    <aside className="w-[260px] bg-sidebar-bg outline outline-card-border">
+    <aside className="sticky top-0 flex h-screen w-[260px] shrink-0 flex-col justify-between overflow-y-scroll scroll-smooth bg-sidebar-bg outline outline-card-border">
       <div className="flex h-20 items-center justify-between px-6">
         <Logo />
         <IconButton className="size-7" variant="outline">
@@ -14,6 +15,7 @@ export default function Sidebar() {
         </IconButton>
       </div>
       <SidebarMenus navData={navData} />
+      <ActiveSuperCard />
     </aside>
   );
 }
