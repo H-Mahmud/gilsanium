51b4ba9993198ed22010dd09673d8bf29aa5ad29
Mahmud <me@imahmud.com>
2025-07-24 15:41:21 +0600

    feat(utils): add currency formatter utils

--- app/utils.ts
+++ app/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatCurrency(
+  value: number,
+  decimalLimit: number = 2,
+  locale: string = 'en-US',
+  currency: string = 'USD',
+) {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency,
+    minimumFractionDigits: decimalLimit,
+    maximumFractionDigits: decimalLimit,
+  }).format(value);
+}
