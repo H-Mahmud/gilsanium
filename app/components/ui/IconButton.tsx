f5258aa0144c2928065370ee2c38ef388799fe78
Mahmud <me@imahmud.com>
2025-07-24 11:12:33 +0600

    style: add guest button hover style

--- app/components/ui/IconButton.tsx
+++ app/components/ui/IconButton.tsx
@@ -13,7 +13,7 @@ export default function IconButton({ variant = 'solid', className, children }: I
         'inline-flex size-8 shrink-0 cursor-pointer items-center justify-center rounded-lg p-2',
         variant === 'outline' && 'border border-icon-border',
         variant === 'solid' && 'bg-primary text-white',
-        variant === 'ghost' && 'bg-transparent',
+        variant === 'ghost' && 'bg-transparent hover:bg-primary/10',
         className,
       )}
     >
