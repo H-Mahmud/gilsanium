1f1a7aeeec86659c56908aeaa23be1dcb98c3315
Mahmud <me@imahmud.com>
2025-07-24 15:13:17 +0600

    style: shop header search and filter design improve

--- app/sections/Shop.tsx
+++ app/sections/Shop.tsx
@@ -1,36 +1,41 @@
 import { Button } from '~/components/ui/Button';
+import Card from '~/components/ui/Card';
 import IconButton from '~/components/ui/IconButton';
 import SVGIcon from '~/components/ui/SVGIcon';
 
 export default function Shop() {
   return (
-    <div>
-      <div className="flex items-center justify-between">
-        <Button rightIcon={<SVGIcon src="/assets/icons/general/ic-open.svg" />} variant="outline">
+    <Card>
+      <div className="flex items-center justify-between gap-2">
+        <Button className="shrink-0" variant="outline">
           For Sale
+          <SVGIcon className="ml-7 size-4" src="/assets/icons/general/ic-open.svg" />
         </Button>
-        <input
-          className="w-full max-w-[300px] rounded-md border border-gray-300 px-3 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
-          placeholder="Search for products..."
-          type="text"
-        />
-        <IconButton>
-          <SVGIcon src="/assets/icons/general/ic-search.svg" />
-        </IconButton>
-        <IconButton>
-          <SVGIcon src="/assets/icons/general/ic-filter.svg" />
+        <div className="flex h-12 w-full basis-full rounded-md border border-gray-300">
+          <input
+            className="h-full w-full px-3 py-2 focus:outline-none"
+            placeholder="Search Shop"
+            type="text"
+          />
+          <IconButton className="h-full w-12" variant="ghost">
+            <SVGIcon className="size-7 bg-primary" src="/assets/icons/general/ic-search.svg" />
+          </IconButton>
+        </div>
+        <IconButton className="size-12 shrink-0" variant="solid">
+          <SVGIcon className="size-7 bg-white" src="/assets/icons/general/ic-filter.svg" />
         </IconButton>
       </div>
 
-      <div className="mt-2 flex items-center justify-between">
+      <div className="mt-3 flex items-center justify-between">
         <span>1-8 of 8 Results</span>
-        <Button rightIcon={<SVGIcon src="/assets/icons/general/ic-open.svg" />} variant="outline">
+        <Button variant="outline">
           Default sort
+          <SVGIcon className="size-4 bg-primary" src="/assets/icons/general/ic-open.svg" />
         </Button>
       </div>
 
-      <div className="mt-2 flex items-stretch justify-between gap-2">
-        <div>Map</div>
+      <div className="mt-6 flex items-stretch justify-between gap-4">
+        <div className="h-[786px] w-5/12 bg-amber-100">Map</div>
         <div>
           <div>
             <img src="/assets/images/products/laptop.png" />
@@ -47,6 +52,6 @@ export default function Shop() {
           </div>
         </div>
       </div>
-    </div>
+    </Card>
   );
 }
