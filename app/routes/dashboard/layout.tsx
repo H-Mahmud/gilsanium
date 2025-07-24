00d6c059689f2a7ed2103793c903fbc55a8f09e0
Mahmud <me@imahmud.com>
2025-07-24 07:57:33 +0600

    chore: move sidebar section to new sidebar folder

--- app/routes/dashboard/layout.tsx
+++ app/routes/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router';
 import Header from '../../sections/Header';
-import Sidebar from '~/sections/Sidebar';
+import { Sidebar } from '~/sections/sidebar';
 
 export default function Layout() {
   return (
