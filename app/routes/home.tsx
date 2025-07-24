ae79735604fbd238246c948a1375786b13244b09
Mahmud <me@imahmud.com>
2025-07-24 18:21:46 +0600

    feat: add dashboard page button

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,16 @@
+import { Link } from 'react-router';
+import { Button } from '~/components/ui/Button';
+
 export function meta() {
   return [{ title: 'Gilsanium | Home' }, { name: 'description', content: 'Welcome to Gilsanium!' }];
 }
 
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-4">
+      <Link to="/dashboard/sales-overview">
+        <Button>Sales Overview Dashboard</Button>
+      </Link>
+    </div>
+  );
 }
