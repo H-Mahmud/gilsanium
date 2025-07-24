63152387708fbc65b2dcd628414bdd96f95b32c9
Mahmud <me@imahmud.com>
2025-07-24 09:12:56 +0600

    chore: add dashboard page paths

---
+++ app/paths.ts
@@ -0,0 +1,25 @@
+export default {
+  dashboard: {
+    root: '/dashboard',
+    overview: '/dashboard/sales-overview',
+    profile: '/dashboard/profile',
+    product: {
+      root: '/dashboard/product',
+      smartwatch: '/dashboard/product/smartwatch',
+      drone: '/dashboard/product/drone',
+      speaker: '/dashboard/product/speaker',
+      chargers: '/dashboard/product/chargers',
+    },
+    customers: {
+      root: '/dashboard/customers',
+      list: '/dashboard/customers/list',
+      new: '/dashboard/customers/new',
+      edit: '/dashboard/customers/edit',
+      account: '/dashboard/customers/account',
+    },
+    message: '/dashboard/message',
+    settings: '/dashboard/settings',
+    help: '/dashboard/help',
+  },
+  logout: '/logout',
+};
