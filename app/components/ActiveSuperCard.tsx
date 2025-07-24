92edb450827512e2dccf5615a5d48999a93059b8
Mahmud <me@imahmud.com>
2025-07-24 16:18:10 +0600

    feat(component): add ActiveSuperCard component

---
+++ app/components/ActiveSuperCard.tsx
@@ -0,0 +1,24 @@
+import Card from './ui/Card';
+import { Button } from './ui/Button';
+
+export default function ActiveSuperCard() {
+  return (
+    <Card className="mx-6 my-10 gap-3 space-y-3 bg-white p-3">
+      <div className="flex items-center justify-between gap-1.5">
+        <img
+          alt="Super Badge"
+          className="size-7 rounded-[6px] stroke-0"
+          src="/assets/badges/super.svg"
+        />
+        <div className="flex w-full basis-full flex-col justify-between">
+          <h3 className="text-sm font-medium">Active Super</h3>
+          <p className="text-[11px]">Unlock All feature on Gilsanium</p>
+        </div>
+      </div>
+
+      <Button className="w-full bg-primary-contrast text-center text-sm font-medium text-white inset-shadow-btn">
+        Upgrade
+      </Button>
+    </Card>
+  );
+}
