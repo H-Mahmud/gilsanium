d214bc511053a1b9989bd8e04dc038cefdec2c41
Mahmud <me@imahmud.com>
2025-08-01 04:57:47 +0600

    feat: implement AnalyticalChart Sales data

--- app/sections/monitoring/AnalyticsChart.tsx
+++ app/sections/monitoring/AnalyticsChart.tsx
@@ -1,60 +1,14 @@
-import {
-  BarChart,
-  Bar,
-  Rectangle,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-} from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import { Button } from '~/components/ui/Button';
 import SVGIcon from '~/components/ui/SVGIcon';
 
 const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
+  { name: 'Jan', Checkout: 3400, Refound: 1200 },
+  { name: 'Feb', Checkout: 1900, Refound: 500 },
+  { name: 'Mar', Checkout: 2800, Refound: 1000 },
+  { name: 'Apr', Checkout: 2000, Refound: 600 },
+  { name: 'May', Checkout: 1700, Refound: 1100 },
+  { name: 'Jun', Checkout: 1300, Refound: 200 },
 ];
 
 export default function AnalyticsChart() {
@@ -67,45 +21,29 @@ export default function AnalyticsChart() {
         </span>
         <div className="inline-flex items-center gap-x-4 text-sm text-primary/80">
           <span>
-            <span className="mr-1 inline-block size-3 bg-[#B9CFF9]" />
+            <span className="mr-1 inline-block size-3 rounded-full bg-[#B9CFF9]" />
             Refound
           </span>
-
           <span>
-            <span className="mr-1 inline-block size-3 bg-primary-contrast" />
+            <span className="mr-1 inline-block size-3 rounded-full bg-[#414ff4]" />
             Checkout
           </span>
           <Button className="ml-8 h-8" variant="outline">
-            This Month
+            This Year
             <SVGIcon className="size-2 bg-primary" src="/assets/icons/general/ic-arrow-down.svg" />
           </Button>
         </div>
       </div>
 
-      <div className="aspect-585/268 w-full">
+      <div className="aspect-585/330 w-full">
         <ResponsiveContainer height="100%" width="100%">
-          <BarChart
-            data={data}
-            height={300}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-            width={500}
-          >
+          <BarChart data={data} height={300} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="name" />
-            <YAxis />
+            <YAxis domain={[0, 3500]} tickCount={6} />
             <Tooltip />
-            <Legend />
-            <Bar activeBar={<Rectangle fill="pink" stroke="blue" />} dataKey="pv" fill="#8884d8" />
-            <Bar
-              activeBar={<Rectangle fill="gold" stroke="purple" />}
-              dataKey="uv"
-              fill="#82ca9d"
-            />
+            <Bar dataKey="Checkout" fill="#414ff4" name="Checkout" radius={[20, 20, 20, 20]} />
+            <Bar dataKey="Refound" fill="#B9CFF9" name="Refound" radius={[20, 20, 20, 20]} />
           </BarChart>
         </ResponsiveContainer>
       </div>
