656c600c1abbb43f1eb88eedb202667c9bed56c8
Mahmud <me@imahmud.com>
2025-08-01 03:48:45 +0600

    refactor: implement reusable AnalyticalCard for summary

--- app/sections/analytical-summary/AnalyticalSummary.tsx
+++ app/sections/analytical-summary/AnalyticalSummary.tsx
@@ -1,5 +1,5 @@
 import AnalyticsFilter from './AnalyticsFilter';
-import AnalyticsCard from '~/sections/analytical-summary/AnalyticsCard';
+import AnalyticsCard from '~/components/AnalyticsCard';
 
 export default function AnalyticalSummary() {
   return (
@@ -7,10 +7,54 @@ export default function AnalyticalSummary() {
       <AnalyticsFilter />
 
       <div className="flex justify-between gap-3">
-        <AnalyticsCard />
-        <AnalyticsCard />
-        <AnalyticsCard />
-        <AnalyticsCard />
+        <AnalyticsCard
+          amount="$108,906"
+          description={
+            <>
+              You earn extra <strong>$5,990</strong> this month
+            </>
+          }
+          icon="money-bag"
+          label="Monthly earning"
+          percentage="5.2%"
+          status="down"
+        />
+        <AnalyticsCard
+          amount="+2,345"
+          description={
+            <>
+              You Received <strong>180</strong> more orders this month
+            </>
+          }
+          icon="shipment-star"
+          label="Total Orders"
+          percentage="8.2%"
+          status="up"
+        />
+        <AnalyticsCard
+          amount="$256,740"
+          description={
+            <>
+              Sales revenue fell by <strong>$10,200</strong> this month
+            </>
+          }
+          icon="shipment-bask"
+          label="Total Sales"
+          percentage="3.8%"
+          status="down"
+        />
+        <AnalyticsCard
+          amount="+1,230"
+          description={
+            <>
+              Gained <strong>65</strong> new customers this month
+            </>
+          }
+          icon="users"
+          label="New Customers"
+          percentage="5.7%"
+          status="up"
+        />
       </div>
     </div>
   );
